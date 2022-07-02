9e3f04b309043ce386945f28938af34d43985155
Ozan <ozan.yasin97@gmail.com>
2022-07-02 03:15:05 +0300

    homework-2 pagination added

--- src/components/data-grid/data-grid.js
+++ src/components/data-grid/data-grid.js
@@ -1,124 +1,145 @@
-import React, { useEffect, useState } from "react"
-import { Button } from "../button"
-import { FormItem } from "../form-item"
+import React, { useEffect, useState } from 'react';
+import { Button } from '../button';
+import { FormItem } from '../form-item';
+import Pagination from '../pagination';
 
 export function DataGrid() {
-
-  const [items, setItems] = useState([])
-  const [loading, setLoading] = useState(false)
-
-  const [todo, setTodo] = useState(null)
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(50);
+  const [todo, setTodo] = useState(null);
 
   useEffect(() => {
-    loadData()
-  }, [])
+    loadData();
+  }, []);
+
+  // Get current items
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const loadData = () => {
-    setLoading(true)
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then(x => x.json())
-      .then(response => {
-        setItems(response)
-        setLoading(false)
-    }).catch(e => {
-      console.log(e)
-      setLoading(false)
-    })
-  }
-
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((x) => x.json())
+      .then((response) => {
+        setItems(response);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setLoading(false);
+      });
+  };
   const renderBody = () => {
     return (
       <React.Fragment>
-        {items.sort((a, b) => b.id - a.id).map((item, i) => {
+        {currentItems.map((item, i) => {
           return (
             <tr key={i}>
-              <th scope="row" >{item.id}</th>
+              <th scope="row">{item.id}</th>
               <td>{item.title}</td>
-              <td>{item.completed ? "Tamamlandı" : "Yapılacak"}</td>
+              <td>{item.completed ? 'Tamamlandı' : 'Yapılacak'}</td>
               <td>
-                <Button className="btn btn-xs btn-danger" onClick={() => onRemove(item.id)}>Sil</Button>
-                <Button className="btn btn-xs btn-warning" onClick={() => onEdit(item)}>Düzenle</Button>
+                <Button
+                  className="btn btn-xs btn-danger"
+                  onClick={() => onRemove(item.id)}
+                >
+                  Sil
+                </Button>
+                <Button
+                  className="btn btn-xs btn-warning"
+                  onClick={() => onEdit(item)}
+                >
+                  Düzenle
+                </Button>
               </td>
             </tr>
-          )
+          );
         })}
       </React.Fragment>
-    )
-  }
+    );
+  };
 
   const renderTable = () => {
     return (
-    <>
-      <Button onClick={onAdd}>Ekle</Button>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Başlık</th>
-            <th scope="col">Durum</th>
-            <th scope="col">Aksiyonlar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {renderBody()}
-        </tbody>
-      </table>
-    </>
-    )
-  }
+      <div>
+        <Button onClick={onAdd}>Ekle</Button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Başlık</th>
+              <th scope="col">Durum</th>
+              <th scope="col">Aksiyonlar</th>
+            </tr>
+          </thead>
+          <tbody>{renderBody()}</tbody>
+        </table>
+        <Pagination
+          itemsPerPage={itemsPerPage}
+          totalItems={items.length}
+          paginate={paginate}
+        />
+      </div>
+    );
+  };
 
   const saveChanges = () => {
-
-    // insert 
+    // insert
     if (todo && todo.id === -1) {
-      todo.id = Math.max(...items.map(item => item.id)) + 1;
-      setItems(items => {
-        items.push(todo)
-        return [...items]
-      })
+      todo.id = Math.max(...items.map((item) => item.id)) + 1;
+      setItems((items) => {
+        items.push(todo);
+        return [...items];
+      });
 
-      alert("Ekleme işlemi başarıyla gerçekleşti.")
-      setTodo(null)
-      return
+      alert('Ekleme işlemi başarıyla gerçekleşti.');
+      setTodo(null);
+      return;
     }
     // update
-    const index = items.findIndex(item => item.id == todo.id)
-    setItems(items => {
-      items[index] = todo
-      return [...items]
-    })
-    setTodo(null)
-  }
+    const index = items.findIndex((item) => item.id == todo.id);
+    setItems((items) => {
+      items[index] = todo;
+      return [...items];
+    });
+    setTodo(null);
+  };
 
   const onAdd = () => {
     setTodo({
       id: -1,
-      title: "",
-      completed: false
-    })
-  }
+      title: '',
+      completed: false,
+    });
+  };
 
   const onRemove = (id) => {
-    const status = window.confirm("Silmek istediğinize emin misiniz?")
+    const status = window.confirm('Silmek istediğinize emin misiniz?');
 
     if (!status) {
-      return
+      return;
     }
-    const index = items.findIndex(item => item.id == id)
-    
-    setItems(items => {
-      items.splice(index, 1)
-      return [...items]
-    })
-  }
+    const index = items.findIndex((item) => item.id == id);
+
+    setItems((items) => {
+      items.splice(index, 1);
+      return [...items];
+    });
+  };
 
   const onEdit = (todo) => {
-    setTodo(todo)
-  }
-  
+    setTodo(todo);
+  };
+
   const cancel = () => {
-    setTodo(null)
-  }
+    setTodo(null);
+  };
 
   const renderEditForm = () => {
     return (
@@ -126,28 +147,31 @@ export function DataGrid() {
         <FormItem
           title="Title"
           value={todo.title}
-          onChange={e => setTodo(todos => {
-            return {...todos, title: e.target.value}
-          })}
+          onChange={(e) =>
+            setTodo((todos) => {
+              return { ...todos, title: e.target.value };
+            })
+          }
         />
         <FormItem
           component="checkbox"
           title="Completed"
           value={todo.completed}
-          onChange={e => setTodo(todos => {
-            return {...todos, completed: e.target.checked}
-          })}
+          onChange={(e) =>
+            setTodo((todos) => {
+              return { ...todos, completed: e.target.checked };
+            })
+          }
         />
         <Button onClick={saveChanges}>Kaydet</Button>
-        <Button className="btn btn-default" onClick={cancel}>Vazgeç</Button>
+        <Button className="btn btn-default" onClick={cancel}>
+          Vazgeç
+        </Button>
       </>
-    )
-  }
-  
+    );
+  };
+
   return (
-    <>
-      { loading ? "Yükleniyor...." : (todo ? renderEditForm() : renderTable())}
-    
-    </>
-  )
+    <>{loading ? 'Yükleniyor....' : todo ? renderEditForm() : renderTable()}</>
+  );
 }
